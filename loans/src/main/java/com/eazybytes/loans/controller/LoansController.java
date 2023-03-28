6c9909e988d14119e633b4cd0afb57996ea5bf16
kaur <kaur@ciklum.com>
2023-03-28 11:23:27 +0500

    Pushing three microservices related to banking application

---
+++ loans/src/main/java/com/eazybytes/loans/controller/LoansController.java
@@ -0,0 +1,38 @@
+/**
+ * 
+ */
+package com.eazybytes.loans.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.eazybytes.loans.model.Loans;
+import com.eazybytes.loans.model.Customer;
+import com.eazybytes.loans.repository.LoansRepository;
+
+/**
+ * @author Eazy Bytes
+ *
+ */
+
+@RestController
+public class LoansController {
+
+	@Autowired
+	private LoansRepository loansRepository;
+
+	@PostMapping("/myLoans")
+	public List<Loans> getLoansDetails(@RequestBody Customer customer) {
+
+		List<Loans> accounts = loansRepository.findByCustomerIdOrderByStartDtDesc(customer.getCustomerId());
+		if (accounts != null) {
+			return accounts;
+		} else {
+			return null;
+		}
+	}
+}
