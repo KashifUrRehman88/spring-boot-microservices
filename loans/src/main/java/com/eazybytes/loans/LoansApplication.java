eebddc0d9e044ff16f6f949bd7b2331490480e31
kaur <kaur@ciklum.com>
2023-07-19 14:08:20 +0500

    Pushing new changes i.e. integerated the configserver to user configurations from github

--- loans/src/main/java/com/eazybytes/loans/LoansApplication.java
+++ loans/src/main/java/com/eazybytes/loans/LoansApplication.java
@@ -15,6 +15,5 @@ public class LoansApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(LoansApplication.class, args);
-		System.out.println("Testing");
 	}
 }
--- loans/src/main/java/com/eazybytes/loans/controller/LoansController.java
+++ loans/src/main/java/com/eazybytes/loans/controller/LoansController.java
@@ -6,13 +6,18 @@ package com.eazybytes.loans.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.eazybytes.loans.model.Loans;
-import com.eazybytes.loans.model.Customer;
+import com.eazybytes.loans.model.*;
+import com.eazybytes.loans.config.LoansServiceConfig;
 import com.eazybytes.loans.repository.LoansRepository;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.ObjectWriter;
 
 /**
  * @author Eazy Bytes
@@ -24,6 +29,9 @@ public class LoansController {
 
 	@Autowired
 	private LoansRepository loansRepository;
+	
+	@Autowired
+	LoansServiceConfig loansServiceConfig;
 
 	@PostMapping("/myLoans")
 	public List<Loans> getLoansDetails(@RequestBody Customer customer) {
@@ -34,5 +42,16 @@ public class LoansController {
 		} else {
 			return null;
 		}
+	}
+	
+	@GetMapping("/loan/properties")
+	public String getPropertyDetails() throws JsonProcessingException {
+		ObjectWriter ow = new ObjectMapper().writer().withDefaultPrettyPrinter();
+		Properties properties = new Properties(loansServiceConfig.getMsg(), loansServiceConfig.getBuildVersion(),
+				loansServiceConfig.getMailDetails(), loansServiceConfig.getActiveBranches());
+		
+		// Using Jackson library, we are trying to convert the properties object into a JSONB format, so that we can display user into a JSON format
+		String jsonStr = ow.writeValueAsString(properties);
+		return jsonStr;
 	}
 }
