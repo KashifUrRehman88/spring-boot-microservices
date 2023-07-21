b8d078baf44db33f06d6f70f41f8e0d8948c1073
kaur <kaur@ciklum.com>
2023-07-21 10:59:04 +0500

    Pushing changes to repoote

--- accounts/src/main/java/com/eazybytes/accounts/AccountsApplication.java
+++ accounts/src/main/java/com/eazybytes/accounts/AccountsApplication.java
@@ -3,11 +3,15 @@ package com.eazybytes.accounts;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.ComponentScans;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@RefreshScope // This annotation is used when change happened in properties and you want to get reflect those changes instead of re-deploy/restart i.e
+// if we used this annotation on individual micro-services, it will enable a new end point on your actuator with name as "/refresh", when ever we invoke this 
+// end point, it will reload the properties without need of re-starting services
 @ComponentScans({ @ComponentScan("com.eazybytes.accounts.controller") })
 @EnableJpaRepositories("com.eazybytes.accounts.repository")
 @EntityScan("com.eazybytes.accounts.model")
