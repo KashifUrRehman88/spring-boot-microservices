87b37ab9782d85541e00406c1f45089b3f223fbc
kaur <kaur@ciklum.com>
2023-07-21 19:05:58 +0500

    Pushing a new microservices i.e. eurekaserver (discovery service)

---
+++ eurekaserver/src/main/java/com/eazybytes/eurekaserver/EurekaserverApplication.java
@@ -0,0 +1,16 @@
+package com.eazybytes.eurekaserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+//Below annotation will make our spring boot project/micro-service to act as a service discovery agent by leveraging eureka component inside it
+@EnableEurekaServer
+public class EurekaserverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EurekaserverApplication.class, args);
+	}
+
+}
