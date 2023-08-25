01bbffabd0d2cc071d2fb21d29b29ae71ed6263b
kaur <kaur@ciklum.com>
2023-08-25 10:18:31 +0500

    Just checking how it is pushing code

--- cards/src/main/java/com/eazybytes/cards/CardsApplication.java
+++ cards/src/main/java/com/eazybytes/cards/CardsApplication.java
@@ -7,7 +7,7 @@ import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.ComponentScans;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
-
+// Just checking how it is pushing code
 @SpringBootApplication
 @RefreshScope // This annotation is used when change happened in properties and you want to get reflect those changes instead of re-deploy/restart i.e
 //if we used this annotation on individual micro-services, it will enable a new end point on your actuator with name as "/refresh", when ever we invoke this 
